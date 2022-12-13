9bc9346940864db2c0ce2096bff1d116a8d65652
mictlantecutli <moisesparra82@hotmail.com>
2022-12-13 02:33:38 -0600

    product data changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import ProductData from "./productData.js";
+import ExternalServices from "./productData.js";
 import ProductList from "./productlist";
 import { loadHeaderFooter } from "./utils.js";
 
 loadHeaderFooter();
 
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices("tents");
 
 const listEle = document.querySelector(".product-list");
 
